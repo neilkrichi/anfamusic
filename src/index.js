f9b1473292a26592904971480bfe6b290aef994e
neilkrichi <neilkrichi@gmail.com>
2018-05-22 19:16:46 -0700

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://anfamusic.herokuapp.com");
+}, 1500000); // every 25 minutes
+
 ReactDOM.render(<Home />, document.getElementById('root'));
