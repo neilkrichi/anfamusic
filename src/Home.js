c1249be2def771fa42cdf1cc818dfb7439cc76ca
neilkrichi <neilkrichi@gmail.com>
2018-05-22 18:42:53 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1 className="app-title">ANFA</h1>
+      </div>
+    );
+  }
+}
