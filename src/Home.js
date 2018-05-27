ff6aa3efe27cf96d225e9485255f1b89cb5da9ea
neilkrichi <neilkrichi@gmail.com>
2018-05-27 00:13:44 -0700

    added typist

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import Typist from 'react-typist';
 import './style.css';
 
+const cursor = {
+  show: true,
+  blink: true,
+  element: '',
+  hideWhenDone: true,
+  hideWhenDoneDelay: 0,
+}
+
 export default class Home extends Component {
   render() {
     return (
       <div className="app">
         <h1 className="app-title">ANFA</h1>
         <div className='contact'>
-          <a href="mailto:license@hivernagerecords.com?subject=Licensing Inquiry">licence@hivernagerecords.com</a>
-          <a href="mailto:press@hivernagerecords.com?subject=Press Inquiry">press@hivernagerecords.com</a>
+          <Typist startDelay={2500} cursor={cursor} avgTypingDelay={50}>
+            <a href="mailto:license@hivernagerecords.com?subject=Licensing Inquiry">licence@hivernagerecords.com</a>
+            </Typist>
+            <Typist startDelay={2500} cursor={cursor} avgTypingDelay={60}>
+            <a href="mailto:press@hivernagerecords.com?subject=Press Inquiry">press@hivernagerecords.com</a>
+          </Typist>
         </div>
       </div>
     );
