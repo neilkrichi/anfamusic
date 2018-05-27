75119ffc04ef57fb898d09be8c4b43d87d9cf540
neilkrichi <neilkrichi@gmail.com>
2018-05-27 03:01:05 -0700

    styles

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Typist from 'react-typist';
+import logo from './assets/logo.eps.svg'
 import './style.css';
 
 const cursor = {
@@ -14,13 +15,13 @@ export default class Home extends Component {
   render() {
     return (
       <div className="app">
-        <h1 className="app-title">ANFA</h1>
+        <img className="app-logo" src={logo} />
         <div className='contact'>
+          <Typist startDelay={2500} cursor={cursor} avgTypingDelay={60}>
+            <a href="mailto:press@hivernagerecords.com?subject=Press Inquiry">Press</a>
+          </Typist>
           <Typist startDelay={2500} cursor={cursor} avgTypingDelay={50}>
-            <a href="mailto:license@hivernagerecords.com?subject=Licensing Inquiry">licence@hivernagerecords.com</a>
-            </Typist>
-            <Typist startDelay={2500} cursor={cursor} avgTypingDelay={60}>
-            <a href="mailto:press@hivernagerecords.com?subject=Press Inquiry">press@hivernagerecords.com</a>
+            <a href="mailto:license@hivernagerecords.com?subject=Licensing Inquiry">License</a>
           </Typist>
         </div>
       </div>
